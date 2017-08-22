b0f30d261bf12eb68c625d15bc26cfbb334673a3
WarManiac <war_lord_1979@msn.com>
2017-08-22 05:29:56 +0200

    Update cached-webpgr.js
    Update:
    add localStorage check -> _loadScript(...) change for support no local localStorage
    add support css file

--- cached-webpgr.js
+++ cached-webpgr.js
@@ -6,8 +6,8 @@
  *
  * usage example:
  *  ```
- *  requireScript('jquery', '1.11.2', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js', function(){
- *    requireScript('examplejs', '0.0.3', 'example.js');
+ *  requireScript('jquery', '1.11.2', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js', false, function(){
+ *    requireScript('examplecss', '0.0.3', 'example.css',true);
  *  });
  *  ```
  */
@@ -19,95 +19,113 @@
  * It will make an ajax call to retrive the desired script from the provided url and store it
  * in the localStorage under the provided name. The stored script will be wrapped like in this example:
  * `{content: '// scrip content $(document).ready(...)', version: '1.02.03'}`
- * @param {string} name localStorage identifier; shoud be the same as on the server-side
- * @param {string} url `path/to/script.js`; shoud be on the same server (or with remove with CORS header access-control-allow-origin:*)
+ * @param {string} url (see `requireScript` or 'css')
+ * @param {string} name (see `requireScript` or 'css')
+ * @param {string} version (see `requireScript` or 'css')
+ * @param {bool} css (false =js, true=css)
+ * @param {Function} callback (see `requireScript` or 'css')
  */
-function _cacheScript(name, version, url) {
-    var xmlhttp = new XMLHttpRequest(); // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState == 4) {
-        if (xmlhttp.status == 200) {
-          localStorage.setItem(name, JSON.stringify({
-            content: xmlhttp.responseText,
-            version: version
-          }));
-        } else {
-          console.warn('error loading '+url);
-        }
-      }
-    }
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-  }
-  /**
+	var dd=document;
+	function _cacheScript(name, version, url, css, callback) {
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function() {
+			if (xmlhttp.readyState == 4) {
+				if (xmlhttp.status == 200) {
+					localStorage.setItem(name, JSON.stringify({
+            			content: xmlhttp.responseText,
+            			version: version
+          			}));
+					_injectScript(name, version, url, css, callback)
+        		} else {
+					if (callback) callback();
+        		}
+      		}
+    	}
+    	xmlhttp.open("GET", url, true);
+    	xmlhttp.send();
+	}
+  /*
    * ##_loadScript
    * For loading external scripts (local or cross domain with CORS)
-   * @param {string} url (see `requireScript`)
-   * @param {string} name (see `requireScript`)
-   * @param {string} version (see `requireScript`)
-   * @param {Function} callback (see `requireScript`)
+   * @param {string} url (see `requireScript` or 'css')
+   * @param {string} name (see `requireScript` or 'css')
+   * @param {string} version (see `requireScript` or 'css')
+   * @param {bool} css (false =js, true=css)
+   * @param {Function} callback (see `requireScript` or 'css')
    */
-function _loadScript(url, name, version, callback) {
-  var s = document.createElement('script');
+	function _loadScript(name, version, url, css, callback) {
+  		var s 
+		if (css)
+			s = dd.createElement('script');
+		else
+			s = dd.createElement('style');
+  		
+		if (s.readyState) {
+    		s.onreadystatechange = function()
+			{
+      			console.log("s.onreadystatechange");
+				if (s.readyState == "loaded" || s.readyState == "complete") {
+        			s.onreadystatechange = null;
+        			if (callback) callback();
+      			}
+    		};
+  		} else {
+			console.log("s.onload");
+    		s.onload = function() {
+      			if (callback) callback();
+    		}
+  		}
+		s.setAttribute("src", url);
+		dd.getElementsByTagName("head")[0].appendChild(s)
+	}
 
-  if (s.readyState) { //IE
-    s.onreadystatechange = function() {
-      if (s.readyState == "loaded" || s.readyState == "complete") {
-        s.onreadystatechange = null;
-        _cacheScript(name, version, url);
-        if (callback) callback();
-      }
-    };
-  } else { //Others
-    s.onload = function() {
-      _cacheScript(name, version, url);
-      if (callback) callback();
-    };
-  }
+  /*
+   * ##_injectScript
+   * Injects a script loaded from localStorage into the DOM.
+   * If the script version is differnt than the requested one, the localStorage key is cleared and a new version will be loaded next time.
+   * @param {string} url (see `requireScript` or 'css')
+   * @param {string} name (see `requireScript` or 'css')
+   * @param {string} version (see `requireScript` or 'css')
+   * @param {bool} css (false =js, true=css)
+   * @param {Function} callback (see `requireScript` or 'css')
+   */
+	function _injectScript(name, version, url, css, callback) {
+  		var c = JSON.parse(localStorage.getItem(name));
+		if (c.version != version) {
+			localStorage.removeItem(name);
+    		_cacheScript(name, version, url, css, callback);
+    		return;
+  		}
+  		var s 
+		if (css)
+			s = dd.createElement('style');
+		else
+			s = dd.createElement('script');
+			
+  		var scriptContent = dd.createTextNode(c.content);
+  		s.appendChild(scriptContent);
+  		dd.getElementsByTagName("head")[0].appendChild(s);
+  		if (callback) callback();
+	}
 
-  s.setAttribute("src", url);
-  document.getElementsByTagName("head")[0].appendChild(s)
-}
-
-/**
- * ##_injectScript
- * Injects a script loaded from localStorage into the DOM.
- * If the script version is differnt than the requested one, the localStorage key is cleared and a new version will be loaded next time.
- * @param {object} content wrapped serialized code `{content: '// scrip content $(document).ready(...)', version: '1.02.03'}`
- * @param {string} name (see `requireScript`)
- * @param {string} version (see `requireScript`)
- * @param {Function} callback (see `requireScript`)
- */
-function _injectScript(content, url, name, version, callback) {
-  var c = JSON.parse(content);
-  // cached version is not the request version, clear the cache, this will trigger a reload next time
-  if (c.version != version) {
-    localStorage.removeItem(name);
-    _loadScript(url, name, version, callback);
-    return;
-  }
-  var s = document.createElement('script');
-  s.type = "text/javascript";
-  var scriptContent = document.createTextNode(c.content);
-  s.appendChild(scriptContent);
-  document.getElementsByTagName("head")[0].appendChild(s);
-  if (callback) callback();
-}
-
-/**
- * ##requireScript
- * If the requested script is not available in the localStorage it will be loaded from the provided url (see `_loadScript`).
- * If the script is present in the localStorage it will be injected (see `_injectScript`) into the DOM.
- * @param {string} name identifier of the script in the local cache
- * @param {string} version version string that is used to check if the script needs to be updated
- * @param {string} url  `path/to/script.js` that should be caced; can be local (or cross domain with CORS header allowing cross domain access)
- * @param {Function} callback function that is extecuted once the script is loaded
- */
-function requireScript(name, version, url, callback) {
-  var c = localStorage.getItem(name);
-  if (c == null) {
-    _loadScript(url, name, version, callback);
-  } else {
-    _injectScript(c, url, name, version, callback);
-  }
-}
+  /*
+   * ##requireScript
+   * If the requested script is not available in the localStorage it will be loaded from the provided url (see `_loadScript`).
+   * If the script is present in the localStorage it will be injected (see `_injectScript`) into the DOM.
+   * @param {string} url (see `requireScript` or 'css')
+   * @param {string} name (see `requireScript` or 'css')
+   * @param {string} version (see `requireScript` or 'css')
+   * @param {bool} css (false =js, true=css)
+   * @param {Function} callback (see `requireScript` or 'css')
+   */
+	function requireScript(name, version, url, css, callback) {
+		if (typeof localStorage === 'undefined')
+		{
+			console.log("localStorage");
+			_loadScript(name, version, url, css, callback)
+		}else if (localStorage.getItem(name) == null) {
+				_cacheScript(name, version, url, css, callback);
+  			  } else {
+				_injectScript(name, version, url, css, callback);
+  			  }
+		}
